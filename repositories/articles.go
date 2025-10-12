a010de34ce8ca369407e46deba2e9802e9a504b3
平林 <sho.2006.hira@gmail.com>
2025-10-12 15:04:56 +0900

    3-8章の問3 SelectArticleDetail関数を実装

--- repositories/articles.go
+++ repositories/articles.go
@@ -6,12 +6,16 @@ import (
 	"myapi/models"
 )
 
+const (
+	articleNumPerPage = 5	
+)
+
 func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	const sqlStr = `
 	insert into articles (title, contents, username, nice, created_at) values
 	(?, ?, ?, 0, now());
 	`
-
+    //ここから問１
 	var newArticle models.Article
 	newArticle.Title, newArticle.Contents, newArticle.UserName = article.Title, article.Contents, article.UserName
 
@@ -21,6 +25,58 @@ func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
 	}
 	id, _ :=result.LastInsertId()
 	newArticle.ID = int(id)
+	//ここまで
 	
     return newArticle, nil
+}
+
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+	    select article_id, title, contents, username, nice
+		from articles
+		limit ? offset ?;
+	`
+	//ここから問２
+    rows, err := db.Query(sqlStr, articleNumPerPage, ((page - 1) * articleNumPerPage))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		articleArray = append(articleArray, article)
+	}
+
+	//ここまで
+	return articleArray, nil
+}
+
+func SelectArticleDetail(db*sql.DB, articleID int) (models.Article, error) {
+	const sqlStr = `
+	    select *
+		from articles
+		where article_id = ?;
+	`
+	//ここから問３
+    row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		return models.Article{}, err
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+	err := row.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum, &createdTime)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+	
+	//ここまで
+	return article, nil
 }
