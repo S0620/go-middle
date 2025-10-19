289a9d957e7fb0a1f44f88e32553dc651147db7d
平林 <sho.2006.hira@gmail.com>
2025-10-19 11:51:45 +0900

    4-4章個別のテストケースごとの後処理を書こう

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -9,7 +9,7 @@ import (
 
 //SelectArticleList関数のテスト
 func TestSelectArticleList(t *testing.T) {
-	expectedNum := 4
+	expectedNum := 2
 	got, err := repositories.SelectArticleList(testDB, 1)
 	if err != nil {
 		t.Fatal(err)
@@ -76,3 +76,34 @@ func TestSelectArticleDetail(t *testing.T) {
 	}
 }
 
+//InsertArticle関数のテスト
+func TestInsertArticle(t *testing.T) {
+	article := models.Article{
+		Title:     "insertTest",
+		Contents:  "testest",
+		UserName:  "saki",
+	}
+
+	expectedArticleNum := 3
+	newArticle, err := repositories.InsertArticle(testDB, article)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if newArticle.ID != expectedArticleNum {
+		t.Errorf("new article id is expected %d but got %d\n", expectedArticleNum, newArticle.ID)
+	}
+
+	t.Cleanup(func() {
+		const sqlStr = `
+			delete from articles
+			where title = ? and contents = ? and username = ?
+		`
+		const sqlReset = `
+			alter table articles auto_increment = 3;
+			`
+
+		testDB.Exec(sqlStr, article.Title, article.Contents, article.UserName)
+		testDB.Exec(sqlReset)
+	})
+}
