1377b90122279238cfa6717fbdd7ed990127e713
平林 <sho.2006.hira@gmail.com>
2025-10-29 19:50:31 +0900

    5-1章PostArticleHandlerのサービスを実装

--- services/article_service.go
+++ services/article_service.go
@@ -29,3 +29,21 @@ func GetArticleService(articleID int) (models.Article, error) {
 
 	return article, nil
 }
+
+
+//PostArticleHandlerで使うことを想定したサービス
+//指定した記事をデータベースに挿入し、挿入後の記事情報を返却
+func PostArticleService(article models.Article) (models.Article, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Article{}, err
+	}
+	defer db.Close()
+
+	newarticle, err := repositories.InsertArticle(db, article)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	return newarticle, nil
+}
