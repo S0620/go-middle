151118f96f5b6642c46104a7e1f1b3fb151125ae
平林 <sho.2006.hira@gmail.com>
2025-10-30 19:11:11 +0900

    5-1章PostNiceHandlerのサービスを実装

--- services/article_service.go
+++ services/article_service.go
@@ -66,3 +66,24 @@ func GetArticleListService(page int) ([]models.Article, error) {
 	return articleList, nil
 }
 
+func PostNiceService(article models.Article) (models.Article, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Article{}, err
+	}
+	defer db.Close()
+
+	err = repositories.UpdateNiceNum(db, article.ID)
+	if err != nil {
+		return models.Article{}, err
+	}
+	return models.Article{
+		ID:        article.ID,
+		Title:     article.Title,
+		Contents:  article.Contents,
+		UserName:  article.UserName,
+		NiceNum:   article.NiceNum + 1,
+		CreatedAt: article.CreatedAt,
+	},nil
+}
+
