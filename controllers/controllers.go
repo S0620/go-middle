fe61e2788e72569330d80ca4b5da8548d0f36a02
平林 <sho.2006.hira@gmail.com>
2025-11-05 13:38:15 +0900

    6-2章サービス層を使う側を大改装の完了

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -18,7 +18,7 @@ func NewMyAppController(s *services.MyAppService) *MyAppController {
 	return &MyAppController{service: s}
 }
 
-func HelloHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 //POST /article のハンドラ
