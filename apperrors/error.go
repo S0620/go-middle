b2ba03b6d7b4c4423a7cfb7963c2627ae2a98b0e
平林 <sho.2006.hira@gmail.com>
2025-11-06 11:09:34 +0900

    7-1章独自エラー型を導入しようを完了

---
+++ apperrors/error.go
@@ -0,0 +1,15 @@
+package apperrors
+
+type MyAppError struct {
+	ErrCode			//レスポンズとログに表示するエラーコード
+	Message string  //レスポンズに表示するエラーメッセージ
+	Err     error   //エラーチェーンのための内部エラー
+}
+
+func (myErr *MyAppError) Error() string {
+	return myErr.Err.Error()
+}
+
+func (myErr *MyAppError) Unwrap() error {
+	return myErr.Err
+}
