abc9b6d307bf63d7fd5cc707ed798259b0e3228c
平林 <sho.2006.hira@gmail.com>
2025-12-12 23:46:15 +0900

    10-3章ゴールーチンを使って並行処理を書いてみる-ロックと待ち合わせ編完了

--- services/article_service.go
+++ services/article_service.go
@@ -6,31 +6,54 @@ import (
 	"myapi/apperrors"
 	"errors"
 	"database/sql"
-	
+	"sync"
 )
 
 //ArticleDetailHandlerで使うことを想定したサービス
 //指定IDの記事情報を返却
 func (s *MyAppService) GetArticleService(articleID int) (models.Article, error) {
+	var article models.Article
+	var commentList []models.Comment
+	var articleGetErr, commentGetErr error
+
+	var amu sync.Mutex
+	var cmu sync.Mutex
+
+	var wg sync.WaitGroup
+	wg.Add(2)
 	
 	//1.repositories層の関数SelectArticleDetailで記事の詳細を取得
-	article, err := repositories.SelectArticleDetail(s.db, articleID)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			err = apperrors.NAData.Wrap(err, "no data")
-			return models.Article{}, err
+	go func(db *sql.DB, articleID int) {
+		defer wg.Done()
+		amu.Lock()
+		article, articleGetErr =  repositories.SelectArticleDetail(db, articleID)
+		amu.Unlock()
+	}(s.db, articleID)
+	
+	go func(db *sql.DB, articleID int) {
+		defer wg.Done()
+		cmu.Lock()
+		commentList, commentGetErr = repositories.SelectCommentList(db, articleID)		
+		cmu.Unlock()
+	}(s.db, articleID)
+
+	wg.Wait()
+	
+	if articleGetErr != nil {
+		if errors.Is(articleGetErr, sql.ErrNoRows) {
+			articleGetErr = apperrors.NAData.Wrap(articleGetErr, "no data")
+			return models.Article{}, articleGetErr
 		}
-		err = apperrors.GetDataFiled.Wrap(err, "fail to get data")
-		return models.Article{}, err
+		articleGetErr = apperrors.GetDataFiled.Wrap(articleGetErr, "fail to get data")
+		return models.Article{}, articleGetErr
 	}
 	//2.コメント一覧を取得
-	commentlist, err := repositories.SelectCommentList(s.db, articleID)
-	if err != nil {
-		err = apperrors.GetDataFiled.Wrap(err, "fail to get data")
+	if commentGetErr != nil {
+		err := apperrors.GetDataFiled.Wrap(commentGetErr, "fail to get data")
 		return models.Article{}, err
 	}
 	//3.2で得たコメント一覧を、1で得たArticle構造体に紐付ける
-	article.CommentList = append(article.CommentList, commentlist...)
+	article.CommentList = append(article.CommentList, commentList...)
 
 	return article, nil
 }
