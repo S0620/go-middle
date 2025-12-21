fedcec6485895e41c03c600dd6a7842122751b59
平林 <sho.2006.hira@gmail.com>
2025-12-21 15:17:22 +0900

    11-2章ログとともに出力するトレースIDを作ろう

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -29,8 +29,10 @@ func (rsw *resLoggingWriter) WriteHeader(code int) {
 //ミドルウェアの中身
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		traceID := NewTraceID()
+		
 		//リクエスト情報をロギング
-		log.Println(req.RequestURI, req.Method)
+		log.Printf("[%d]%s %s\n", traceID, req.RequestURI, req.Method)
 
 		//自作のResponseWriterを作って
 		rlw := NewResLoggingWriter(w)
@@ -39,6 +41,6 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(rlw, req)
 
 		//自作ResponseWriterからロギングしたいデータを出す
-		log.Println("res: ", rlw.code)
+		log.Printf("[%d]res: %d", traceID, rlw.code)
 	}) 
 }
